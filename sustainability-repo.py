d204c1107fd4f910683055de9487c7a38ac2be03
teamcharliegithub <55711242+teamcharliegithub@users.noreply.github.com>
2021-03-28 02:38:45 -0400

    Update sustainability-repo.py
    Removed unnecessary comment at beginning of file.

--- sustainability-repo.py
+++ sustainability-repo.py
@@ -1,14 +1,3 @@
-'''
-Technical Description of Autonomous Wick Irrigation Community Garden System: Our system has three parts.
-A composting system, a rainwater reservoir system, and a wick irrigation system.
-Our composting system will take all carbon and nitrogen related inputs from the community in the form of food scraps,
-lawn clippings, etc. and Composting System We will be using vermicomposting systems for our community gardens as they
-are much more efficient than traditional aerobic composting systems as they take less time, and the worms within the
-bins can self-multiply within a relatively short time frame if more people use the worm bin.
-itself when compared will consist of multiple horizontally oriented worm bins located next to Rudimentary Rain Reservoir
-System.
-'''
-
 import time
 import pyfirmata
 from datetime import datetime
